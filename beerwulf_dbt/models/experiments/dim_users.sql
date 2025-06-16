9527af457d36a501d07d127e459da6a7cb803723
Sajad Ahmadi Jozdani <61582647+sajadahmadia@users.noreply.github.com>
2025-06-16 20:46:37 +0200

    source moved to real seed

--- beerwulf_dbt/models/experiments/dim_users.sql
+++ beerwulf_dbt/models/experiments/dim_users.sql
@@ -9,5 +9,5 @@
 select user_id,
     max(platform) platform,
     current_timestamp as updated_at
-from {{source('seeds','user_events_mock_enriched')}}
+from {{ref('user_events_mock_enriched')}}
 group by user_id
